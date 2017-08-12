ea2aad9b313d06d28c1e669fa444ded50e68a2b0
Andrey <3llianAstra@gmail.com>
2017-08-13 02:43:49 +0300

    javase01.t04 done

--- src/main/java/javase01/t04/Array.java
+++ src/main/java/javase01/t04/Array.java
@@ -1,9 +1,18 @@
 package javase01.t04;
 
+import com.sun.istack.internal.NotNull;
+
 class Array {
 
     static double maxVal(double[] a) {
-        double max = 0;
+        double max = Double.MIN_VALUE;
+        double[] sum = new double[a.length / 2];
+        for (int i = 0; i < sum.length; i++ ) {
+            sum[i] = a[i] + a[a.length - i - 1];
+            System.out.println();
+            if (sum[i] > max)
+                max = sum[i];
+        }
         return max;
     }
 }
