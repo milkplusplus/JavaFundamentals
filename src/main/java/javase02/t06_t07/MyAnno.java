398c98ea53000cfa69adc5da68555d37ff38db58
Andrey <3llianAstra@gmail.com>
2017-08-15 01:29:20 +0300

    t06 started, NuclearSubmarine class added

---
+++ src/main/java/javase02/t06_t07/MyAnno.java
@@ -0,0 +1,4 @@
+package javase02.t06_t07;
+
+public @interface MyAnno {
+}
---
+++ src/main/java/javase02/t06_t07/NuclearSubmarine.java
@@ -0,0 +1,31 @@
+package javase02.t06_t07;
+
+public class NuclearSubmarine {
+
+    private NuclearEngine nuclearEngine = new NuclearEngine();
+    private int nuclearFuelLevel;
+
+    public int getNuclearFuelLevel() {
+        return nuclearFuelLevel;
+    }
+
+    public void setNuclearFuelLevel(int nuclearFuelLevel) {
+        this.nuclearFuelLevel = nuclearFuelLevel;
+    }
+
+    class NuclearEngine {
+        private boolean start() {
+            if (nuclearFuelLevel > 0){
+                nuclearFuelLevel--;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    void takeATrip() {
+        System.out.println(nuclearEngine.start() ?
+                "Good luck in the journey!" :
+                "Not today - out of fuel :(");
+    }
+}
