f0151fc826ccb9a7edf7db7304f8feef5ad10c6d
Andrey <3llianAstra@gmail.com>
2017-08-14 22:49:42 +0300

    Finished Pencil and abstract Stationery classes

--- src/main/java/javase02/t03/Pencil.java
+++ src/main/java/javase02/t03/Pencil.java
@@ -1,4 +1,29 @@
 package javase02.t03;
 
 public class Pencil extends Stationery {
+
+    private String color;
+    private String graphiteGradingScale;
+
+    public Pencil(String name, int cost, String color, String graphiteGradingScale) {
+        super(name, cost);
+        this.color = color;
+        this.graphiteGradingScale = graphiteGradingScale;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getGraphiteGradingScale() {
+        return graphiteGradingScale;
+    }
+
+    public void setGraphiteGradingScale(String graphiteGradingScale) {
+        this.graphiteGradingScale = graphiteGradingScale;
+    }
 }
--- src/main/java/javase02/t03/Stationery.java
+++ src/main/java/javase02/t03/Stationery.java
@@ -2,4 +2,27 @@ package javase02.t03;
 
 public abstract class Stationery {
 
+    private String name;
+    private int cost;
+
+    Stationery(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    int getCost() {
+        return cost;
+    }
 }
