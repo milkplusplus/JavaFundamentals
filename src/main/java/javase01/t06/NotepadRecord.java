e4593aab305950dcf53e91d5457255aa3724db3b
Andrey <3llianAstra@gmail.com>
2017-08-14 03:01:13 +0300

    Class NotepadRecord done

--- src/main/java/javase01/t06/NotepadRecord.java
+++ src/main/java/javase01/t06/NotepadRecord.java
@@ -1,4 +1,18 @@
 package javase01.t06;
 
 public class NotepadRecord {
+
+    private String note;
+
+    public NotepadRecord(String note) {
+        this.note = note;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
