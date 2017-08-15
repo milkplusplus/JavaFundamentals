7ec85be1e6397ecdc22fed4af942f08ccfc03d04
Andrey <3llianAstra@gmail.com>
2017-08-15 17:12:53 +0300

    More bug fixes

--- src/main/java/javase02/t05/Discipline.java
+++ src/main/java/javase02/t05/Discipline.java
@@ -44,31 +44,76 @@ public enum Discipline {
     }
 
     private static DisciplineListener findBestListenerGrade(LinkedList<DisciplineListener> gradeList) {
-            gradeList.sort(Comparator.comparing(DisciplineListener::getDiscipline));
+            gradeList.sort(Comparator.comparingInt(d -> d.getDiscipline().getGradeType().ordinal()));
             LinkedList<DisciplineListener> IntegerGradeList = new LinkedList<>();
             LinkedList<DisciplineListener> DoubleGradeList = new LinkedList<>();
-            DisciplineListener current = gradeList.getFirst();
             int counter = 0;
-            for (; (counter < gradeList.size()) && (current.getDiscipline().gradeType != GradeType.DECIMAL); counter++)
-                IntegerGradeList.add(current);
-            for (; (counter < gradeList.size()) && (current.getDiscipline().gradeType != GradeType.DECIMAL); counter++)
-                DoubleGradeList.add(current);
+            for (; (counter < gradeList.size()) && (gradeList.get(counter).getDiscipline().getGradeType() == GradeType.INTEGER); counter++) {
+                IntegerGradeList.add(gradeList.get(counter));
+            }
+            for (; counter < gradeList.size(); counter++) {
+                DoubleGradeList.add(gradeList.get(counter));
+            }
             if ((IntegerGradeList.size() == 0) || (DoubleGradeList.size() == 0)) {
                  if (IntegerGradeList.size() == 0){
-                     DoubleGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) -> (Double.compare((Double)lis1.getGrade(), (Double)lis2.getGrade())));
+                     DoubleGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
+                             (Double.compare((Double)lis1.getGrade(), (Double)lis2.getGrade())));
                      return DoubleGradeList.getLast();
                  }
                  else {
-                     IntegerGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) -> (Integer)lis1.getGrade() - (Integer) lis2.getGrade());
+                     IntegerGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
+                             (Integer)lis1.getGrade() - (Integer) lis2.getGrade());
                      return IntegerGradeList.getLast();
                  }
             }
-            DoubleGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) -> (Double.compare((Double)lis1.getGrade(), (Double)lis2.getGrade())));
-            IntegerGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) -> (Integer)lis1.getGrade() - (Integer) lis2.getGrade());
-            if ( Double.compare(IntegerGradeList.getLast().getGrade().doubleValue(), DoubleGradeList.getLast().getGrade().doubleValue()) >= 0 )
+            DoubleGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
+                    (Double.compare((Double)lis1.getGrade(), (Double)lis2.getGrade())));
+            IntegerGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
+                    (Integer)lis1.getGrade() - (Integer) lis2.getGrade());
+            if ( Double.compare(IntegerGradeList.getLast().getGrade().doubleValue(),
+                    DoubleGradeList.getLast().getGrade().doubleValue()) >= 0 )
                 return IntegerGradeList.getLast();
             return DoubleGradeList.getLast();
     }
+
+
+//        gradeList.sort(Comparator.comparingInt(d -> d.getDiscipline().getGradeType().ordinal()));
+//        DisciplineListener maxInteger = null;
+//        DisciplineListener maxDouble = null;
+//        DisciplineListener current = gradeList.getFirst();
+//        int counter = 0;
+//
+//
+//        for (; (counter < gradeList.size()) && (current.getDiscipline().gradeType == GradeType.INTEGER); counter++) {
+//            if ()
+//            IntegerGradeList.add(current);
+//            current = gradeList.get(counter);
+//        }
+//        for (; counter < gradeList.size(); counter++) {
+//            DoubleGradeList.add(current);
+//            current = gradeList.get(counter);
+//        }
+//        if ((IntegerGradeList.size() == 0) || (DoubleGradeList.size() == 0)) {
+//            if (IntegerGradeList.size() == 0){
+//                DoubleGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
+//                        (Double.compare((Double)lis1.getGrade(), (Double)lis2.getGrade())));
+//                return DoubleGradeList.getLast();
+//            }
+//            else {
+//                IntegerGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
+//                        (Integer)lis1.getGrade() - (Integer) lis2.getGrade());
+//                return IntegerGradeList.getLast();
+//            }
+//        }
+//        DoubleGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
+//                (Double.compare((Double)lis1.getGrade(), (Double)lis2.getGrade())));
+//        IntegerGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
+//                (Integer)lis1.getGrade() - (Integer) lis2.getGrade());
+//        if ( Double.compare(IntegerGradeList.getLast().getGrade().doubleValue(),
+//                DoubleGradeList.getLast().getGrade().doubleValue()) >= 0 )
+//            return IntegerGradeList.getLast();
+//        return DoubleGradeList.getLast();
+//    }
 
     public GradeType getGradeType() {
         return gradeType;
