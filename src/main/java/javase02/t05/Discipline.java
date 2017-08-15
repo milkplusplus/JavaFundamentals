b79d961ecd930a20160d018972ac94a517437d33
Andrey <3llianAstra@gmail.com>
2017-08-15 17:23:20 +0300

    Added comment, created test class

--- src/main/java/javase02/t05/Discipline.java
+++ src/main/java/javase02/t05/Discipline.java
@@ -43,17 +43,18 @@ public enum Discipline {
         return null;
     }
 
+    // need to optimize this function later
+    // (dunno why i decided to realize this with LinkedList instead of single DisciplineListener)
     private static DisciplineListener findBestListenerGrade(LinkedList<DisciplineListener> gradeList) {
             gradeList.sort(Comparator.comparingInt(d -> d.getDiscipline().getGradeType().ordinal()));
             LinkedList<DisciplineListener> IntegerGradeList = new LinkedList<>();
             LinkedList<DisciplineListener> DoubleGradeList = new LinkedList<>();
             int counter = 0;
-            for (; (counter < gradeList.size()) && (gradeList.get(counter).getDiscipline().getGradeType() == GradeType.INTEGER); counter++) {
+            for (; (counter < gradeList.size()) && (gradeList.get(counter).getDiscipline().getGradeType() ==
+                    GradeType.INTEGER); counter++)
                 IntegerGradeList.add(gradeList.get(counter));
-            }
-            for (; counter < gradeList.size(); counter++) {
+            for (; counter < gradeList.size(); counter++)
                 DoubleGradeList.add(gradeList.get(counter));
-            }
             if ((IntegerGradeList.size() == 0) || (DoubleGradeList.size() == 0)) {
                  if (IntegerGradeList.size() == 0){
                      DoubleGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
@@ -75,45 +76,6 @@ public enum Discipline {
                 return IntegerGradeList.getLast();
             return DoubleGradeList.getLast();
     }
-
-
-//        gradeList.sort(Comparator.comparingInt(d -> d.getDiscipline().getGradeType().ordinal()));
-//        DisciplineListener maxInteger = null;
-//        DisciplineListener maxDouble = null;
-//        DisciplineListener current = gradeList.getFirst();
-//        int counter = 0;
-//
-//
-//        for (; (counter < gradeList.size()) && (current.getDiscipline().gradeType == GradeType.INTEGER); counter++) {
-//            if ()
-//            IntegerGradeList.add(current);
-//            current = gradeList.get(counter);
-//        }
-//        for (; counter < gradeList.size(); counter++) {
-//            DoubleGradeList.add(current);
-//            current = gradeList.get(counter);
-//        }
-//        if ((IntegerGradeList.size() == 0) || (DoubleGradeList.size() == 0)) {
-//            if (IntegerGradeList.size() == 0){
-//                DoubleGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
-//                        (Double.compare((Double)lis1.getGrade(), (Double)lis2.getGrade())));
-//                return DoubleGradeList.getLast();
-//            }
-//            else {
-//                IntegerGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
-//                        (Integer)lis1.getGrade() - (Integer) lis2.getGrade());
-//                return IntegerGradeList.getLast();
-//            }
-//        }
-//        DoubleGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
-//                (Double.compare((Double)lis1.getGrade(), (Double)lis2.getGrade())));
-//        IntegerGradeList.sort( (DisciplineListener lis1, DisciplineListener lis2) ->
-//                (Integer)lis1.getGrade() - (Integer) lis2.getGrade());
-//        if ( Double.compare(IntegerGradeList.getLast().getGrade().doubleValue(),
-//                DoubleGradeList.getLast().getGrade().doubleValue()) >= 0 )
-//            return IntegerGradeList.getLast();
-//        return DoubleGradeList.getLast();
-//    }
 
     public GradeType getGradeType() {
         return gradeType;
---
+++ src/test/java/javase02/t05/DisciplineTest.java
@@ -0,0 +1,21 @@
+package javase02.t05;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DisciplineTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addListener() throws Exception {
+    }
+
+    @Test
+    public void bestStudentGradle() throws Exception {
+    }
+
+}
