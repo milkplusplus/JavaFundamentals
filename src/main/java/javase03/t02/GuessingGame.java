6155744b907b53343c9e893e7b1ae995208f19be
Andrey <3llianAstra@gmail.com>
2017-08-17 02:44:38 +0300

    javase03.t02 class working now and added properties for him

--- src/main/java/javase03/t02/GuessingGame.java
+++ src/main/java/javase03/t02/GuessingGame.java
@@ -1,9 +1,19 @@
 package javase03.t02;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
 public class GuessingGame {
 
     private void start(){
-
+        Scanner in = new Scanner(System.in);
+        System.out.print("Choose your language (English - 1, Russian - 2): ");
+        Locale locale = new Locale(in.nextInt() == 2 ? "ru" : "en");
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("guessinggame", locale);
+        System.out.println(resourceBundle.getObject("q1"));
+        System.out.println(resourceBundle.getObject("a1"));
+        in.close();
     }
 
     public static void main(String[] args) {
