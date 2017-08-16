95e11707a287b70c38ab4640fc6966a0603a8526
Andrey <3llianAstra@gmail.com>
2017-08-16 23:45:25 +0300

    Added searchContains method

--- src/main/java/javase03/t01/CrazyLogger.java
+++ src/main/java/javase03/t01/CrazyLogger.java
@@ -1,9 +1,8 @@
 package javase03.t01;
 
-
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
 
 @SuppressWarnings("WeakerAccess")
 public class CrazyLogger {
@@ -20,10 +19,24 @@ public class CrazyLogger {
 
     public void addMess(String message) {
         if (null != message)
-            stringBuilder.append(String.format("%s - %s",
-                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-YYYY : hh-mm")),
+            stringBuilder.append(String.format("%s - %s; \n",
+                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-YYYY : HH-mm")),
                     message));
     }
 
+    public LinkedList<String> searchContains(String target) {
+        LinkedList<String> searched = new LinkedList<>();
+        String [] strings = stringBuilder.toString().split("\n");
+        for (String i: strings)
+            if (i.contains(target))
+                searched.add(i);
+        return searched;
+    }
 
+    public LinkedList<String> searchExactMatch(String target) {
+        LinkedList<String> searched = new LinkedList<>();
+        String [] strings = stringBuilder.toString().split("\n");
+
+        return searched;
+    }
 }
