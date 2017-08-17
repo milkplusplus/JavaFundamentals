7e6aad057687c0d9d2b31906ec063fa5fc6bc437
Andrey <3llianAstra@gmail.com>
2017-08-17 03:46:30 +0300

    Added comments and javase03.t02 is done now

--- src/main/java/javase03/t02/GuessingGame.java
+++ src/main/java/javase03/t02/GuessingGame.java
@@ -6,14 +6,36 @@ import java.util.Scanner;
 
 public class GuessingGame {
 
-    private void start(){
+    private void start() {
         Scanner in = new Scanner(System.in);
         System.out.print("Choose your language (English - 1, Russian - 2): ");
         Locale locale = new Locale(in.nextInt() == 2 ? "ru" : "en");
         ResourceBundle resourceBundle = ResourceBundle.getBundle("guessinggame", locale);
-        System.out.println(resourceBundle.getObject("q1"));
-        System.out.println(resourceBundle.getObject("a1"));
+        //"index + 1" because in properties we start from q1
+        for (int index = 0; index < getCountOfQuestions(resourceBundle); index++)
+            System.out.println(String.format("%d) %s",  index + 1, resourceBundle.getObject(String.format("q%d",index + 1))));
+        System.out.print("\n" + resourceBundle.getObject("i"));
+        int key = in.nextInt();
+        while (key != 0) {
+            System.out.println(resourceBundle.getObject(String.format("q%d", key)));
+            System.out.println(resourceBundle.getObject(String.format("a%d", key)));
+            System.out.print("\n" + resourceBundle.getObject("i"));
+            key = in.nextInt();
+        }
         in.close();
+    }
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    private int getCountOfQuestions (ResourceBundle resourceBundle) {
+        int count = 0;
+        try {
+            while (true) {
+                resourceBundle.getObject(String.format("q%d", count + 1));
+                count++;
+            }
+        } catch (java.util.MissingResourceException e) {
+            return count;
+        }
     }
 
     public static void main(String[] args) {
