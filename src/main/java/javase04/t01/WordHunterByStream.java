e911236adf637a4612b7259085a6486a6c34d1bd
Andrey <3llianAstra@gmail.com>
2017-08-17 23:13:22 +0300

    javase04.t01 working now

--- src/main/java/javase04/t01/WordHunterByStream.java
+++ src/main/java/javase04/t01/WordHunterByStream.java
@@ -1,5 +1,9 @@
 package javase04.t01;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class WordHunterByStream {
 
     static final String[] JAVA_WORDS = {
@@ -15,6 +19,34 @@ public class WordHunterByStream {
             "transient", "try", "void", "volatile", "while"};
 
     public static void findWords(String pathFrom, String pathTo) {
+        byte[] bytesBuff;
+        int bytesAvlbl = 0;
+        int overallCounter = 0;
+
+
+        try (FileInputStream in = new FileInputStream(pathFrom); FileOutputStream out = new FileOutputStream(pathTo)) {
+            while((bytesAvlbl = in.available()) != 0) {
+
+                bytesBuff = new byte[bytesAvlbl];
+                in.read(bytesBuff, 0, bytesAvlbl);
+                String[] buffArray = new String(bytesBuff).split("\\s+");
+
+
+                for (int i = 0; i < JAVA_WORDS.length; i++) {
+                    int localCounter = 0;
+                    for (String buff : buffArray) {
+                        if (JAVA_WORDS[i].equals(buff))
+                            localCounter++;
+                    }
+                    if (localCounter > 0) {
+                        out.write(String.format("%s %d \n", JAVA_WORDS[i], localCounter).getBytes("UTF-8"));
+                        overallCounter += localCounter;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
