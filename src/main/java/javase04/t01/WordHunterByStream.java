2cf0084545fe4150860bdd95761aac202c3ffdb5
Andrey <3llianAstra@gmail.com>
2017-08-17 22:29:12 +0300

    Started javase04.t01, added String array with java words

---
+++ src/main/java/javase04/t01/WordHunterByStream.java
@@ -0,0 +1,21 @@
+package javase04.t01;
+
+public class WordHunterByStream {
+
+    static final String[] JAVA_WORDS = {
+            "abstract", "assert", "boolean", "break", "byte",
+            "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else",
+            "enum", "extends", "final", "finally", "float",
+            "for", "goto", "if", "implements", "import",
+            "instanceof", "int", "interface", "long", "native",
+            "new", "package", "private", "protected", "public",
+            "return", "short", "static", "strictfp", "super",
+            "switch", "synchronized", "this", "throw", "throws",
+            "transient", "try", "void", "volatile", "while"};
+
+    public static void findWords(String pathFrom, String pathTo) {
+
+    }
+}
+
