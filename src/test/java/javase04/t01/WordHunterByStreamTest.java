0d48081a140302b5935729a034beffafba84fbf7
Andrey <3llianAstra@gmail.com>
2017-08-17 23:43:13 +0300

    javase04.t01 done, test added

---
+++ src/test/java/javase04/t01/WordHunterByStreamTest.java
@@ -0,0 +1,15 @@
+package javase04.t01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordHunterByStreamTest {
+
+    @Test
+    public void findWords() {
+        assertEquals(27, WordHunterByStream.findWords("src/test/resources/TestFromjavase04.t01.txt",
+                "src/test/resources/TestTojavase04.t01.txt"));
+    }
+
+}
